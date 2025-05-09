af0ffc9195c0e3d6bac7afc1a898e85ee938afb4
Daniel Yudelevich <dyudelevich@madobjective.com>
2025-05-08 18:16:11 -0700

    fix(doublemetaphone.pyx): change function parameter type from basestring to object for better compatibility fix(doublemetaphone.pyx): update _bstring function to handle different string types more robustly

--- doublemetaphone/doublemetaphone.pyx
+++ doublemetaphone/doublemetaphone.pyx
@@ -6,15 +6,16 @@ from libcpp.vector cimport vector
 cdef extern from "double_metaphone.h" nogil :
     void DoubleMetaphone(string s, vector[string] *c)
 
-cpdef tuple doublemetaphone(basestring s):
+cpdef tuple doublemetaphone(object s):
     cdef string cpp_string = _bstring(s)
     cdef vector[string] codes
     DoubleMetaphone(cpp_string, &codes)
     return codes[0].c_str().decode('utf-8'), codes[1].c_str().decode('utf-8')
 
-cdef bytes _bstring(basestring s):
-    if type(s) is unicode:
-        # fast path for most common case(s)
+cdef bytes _bstring(object s):
+    if isinstance(s, str):
         return s.encode('utf-8')
-    else : # safe because of basestring
-        return <char *>s
+    elif isinstance(s, bytes):
+        return s
+    else:
+        return str(s).encode('utf-8')
